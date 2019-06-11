3ba10afaa43a3d9af508d5759edd01b6b81baa1a
AungZinTun <aungzintun2@gmail.com>
2019-06-11 16:33:37 +0630

    first

--- shd/settings.py
+++ shd/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+import django_heroku
 import os
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,3 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 Static_root=os.path.join(BASE_DIR, 'static'),
 STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static'),]
+
+
+django_heroku.settings(locals())
