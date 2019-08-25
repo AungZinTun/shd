e64d822080ff3b7aaf2fcaf0615749edd72aa755
AungZinTun <aungzintun2@gmail.com>
2019-08-25 08:08:44 +0630

    tb

--- TB/admin.py
+++ TB/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import tb, township
 from import_export import resources
-from import_export.admin import ImportExportModelAdmin
+# from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 
 class tb_inline(admin.TabularInline):
@@ -9,8 +9,8 @@ class tb_inline(admin.TabularInline):
 class tsp_tb(admin.ModelAdmin):
     inlines=[ tb_inline, ]
 
-@admin.register(tb)
-class ViewAdini(ImportExportModelAdmin):
-    pass
+# @admin.register(tb)
+# class ViewAdini(ImportExportModelAdmin):
+#     pass
 
 admin.site.register(township, tsp_tb)
