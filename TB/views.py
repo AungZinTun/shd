0d203dd672cfffdc028ac74b6fee6fc1b4d8e0db
AungZinTun <aungzintun2@gmail.com>
2019-08-25 08:20:42 +0630

    tb

--- TB/views.py
+++ TB/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import tb, township
 from random import randint
 from django.views.generic import TemplateView
-from chartjs.views.lines import BaseLineChartView
+
 
 
 def index(request):
